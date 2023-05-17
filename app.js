963f87c6b196a6e6e1017b660ec91d426b75ae4a
gary909 <garywhite61@hotmail.com>
2023-05-17 12:11:32 +0200

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+// return the two oldest/oldest ages within the array of ages passed in.
+function twoOldestAges(ages){
+
+}
+
+console.log(twoOldestAges([1,5,87,45,8,8])); // [45, 87]
+console.log(twoOldestAges([6,5,83,5,3,18])); // [18, 83]
