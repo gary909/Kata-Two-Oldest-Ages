ce17f84675f4d0ae413793eccd2dcc191993c880
gary909 <garywhite61@hotmail.com>
2023-05-17 12:16:36 +0200

    code working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // return the two oldest/oldest ages within the array of ages passed in.
 function twoOldestAges(ages){
-
+    let sortedArr = ages.sort(function(a, b){return b-a}); // sort into descending
+    return [sortedArr[1], sortedArr[0]];
 }
 
 console.log(twoOldestAges([1,5,87,45,8,8])); // [45, 87]
